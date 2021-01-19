0962bf97231eff75e7b7b42423a6b041e4ed895f
Rostislav-callback <rost33445500@gmail.com>
2021-01-19 17:49:24 +0200

    commit1

---
+++ src/app/arganizer/arganizer.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { switchMap } from 'rxjs/operators';
+import { DateService } from '../shared/date.service';
+import { Task, TasksService } from '../shared/tasks.service';
+
+@Component({
+  selector: 'app-arganizer',
+  templateUrl: './arganizer.component.html',
+  styleUrls: ['./arganizer.component.scss']
+})
+export class ArganizerComponent implements OnInit {
+
+  form: FormGroup
+  tasks: Task[] = []
+
+  constructor(public dateService: DateService,
+              private taskService: TasksService) { }
+
+  ngOnInit(): void {
+    this.dateService.date.pipe(
+      switchMap(value => this.taskService.load(value))
+    ).subscribe(tasks => {
+      this.tasks = tasks
+    })
+
+    this.form = new FormGroup({
+      title: new FormControl('', Validators.required)
+    })
+  }
+
+  submit() {
+    const {title} = this.form.value
+
+    const task: Task = {
+      title,
+      date: this.dateService.date.value.format('DD-MM-YYYY')
+    }
+    
+    this.taskService.create(task).subscribe(task => {
+      this.tasks.push(task)
+      this.form.reset()
+    }, err => console.error(err)) 
+  }
+
+  remove(task: Task) {
+    this.taskService.remove(task).subscribe(() => {
+      this.tasks = this.tasks.filter(t => t.id !== task.id)
+    }, err => console.error(err))
+  }
+}
